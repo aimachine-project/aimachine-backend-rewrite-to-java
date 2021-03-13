86377f9c03c4dad7ff7341fed2aca5c9e43c556e
Sebastian Syska <syska.seb@gmail.com>
2021-03-13 13:51:27 +0100

    simple working socketio server

--- src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
+++ src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
@@ -1,10 +1,32 @@
 package ai.aimachineserver
 
+import com.corundumstudio.socketio.Configuration
+import com.corundumstudio.socketio.SocketIOServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class AimachineServerApplication
+class AimachineServerApplication {
+
+    @Bean
+    fun socketIOServer(): SocketIOServer? {
+        val config = Configuration()
+        config.hostname = "localhost"
+        config.port = 9090
+        val server = SocketIOServer(config)
+
+        server.addConnectListener {
+            println("Client connected")
+        }
+
+        server.addDisconnectListener {
+            println("client disconnected")
+        }
+
+        return server
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<AimachineServerApplication>(*args)
--- src/main/resources/static/js/ConnectionHandler.js
+++ src/main/resources/static/js/ConnectionHandler.js
@@ -1,5 +1,5 @@
 class ConnectionHandler {
-    static #serverUrl = location.protocol + "//" + document.domain + ":" + location.port;
+    static #serverUrl = location.protocol + "//" + document.domain + ":" + 9090;
     static #connectionOptions = {autoConnect: false, reconnection: false};
 
     #socket = null;
