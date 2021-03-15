8c7a95c7720f0d6119140039be90303ecb495062
Sebastian Syska <syska.seb@gmail.com>
2021-03-15 21:39:10 +0100

    origin policy

--- src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
+++ src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
@@ -16,6 +16,7 @@ class AimachineServerApplication {
     @Bean
     fun socketIOServer(): SocketIOServer? {
         val config = Configuration()
+        config.origin = "*"
         config.hostname = "0.0.0.0"
         config.port = 9000
         val server = SocketIOServer(config)
