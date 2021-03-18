a2cc71b8e5ae3ae8a49257a43d528f96f457d00f
Sebastian Syska <syska.seb@gmail.com>
2021-03-18 19:56:28 +0100

    cors on heroku app url

--- src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
+++ src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
@@ -18,7 +18,7 @@ class AimachineServerApplication {
         val config = Configuration()
         config.hostname = "0.0.0.0"
         config.port = 9000
-        config.origin = "https://aimachine-backend.herokuapp.com:9000"
+        config.origin = "http://aimachine-backend.herokuapp.com"
 
         val server = SocketIOServer(config)
 
