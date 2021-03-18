16a71b812cbe279f1530234d118d70bc03262924
Sebastian Syska <syska.seb@gmail.com>
2021-03-18 20:04:11 +0100

    server origin ref

--- src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
+++ src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
@@ -18,7 +18,7 @@ class AimachineServerApplication {
         val config = Configuration()
         config.hostname = "0.0.0.0"
         config.port = 9000
-        config.origin = "http://aimachine-backend.herokuapp.com"
+        config.origin = "*/*"
 
         val server = SocketIOServer(config)
 
