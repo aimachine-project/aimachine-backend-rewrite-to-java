95704b69d816ee01f719ee7d9acaef7f1f987594
Sebastian Syska <syska.seb@gmail.com>
2021-03-21 18:19:46 +0100

    extracted cors configurer to class

--- src/main/kotlin/ai/aimachineserver/CorsConfig.kt
+++ src/main/kotlin/ai/aimachineserver/CorsConfig.kt
@@ -1,22 +1,20 @@
 package ai.aimachineserver
 
-import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Bean
-fun corsConfigurer(): WebMvcConfigurer {
-    return object : WebMvcConfigurer {
-        override fun addCorsMappings(registry: CorsRegistry) {
-            registry
-                .addMapping("/**")
-                .allowedOrigins("https://aimachine-backend.herokuapp.com")
-                .exposedHeaders(
-                    "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                    "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-                    "Cache-Control", "Content-Type"
-                )
-                .allowCredentials(true)
-        }
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("https://aimachine-backend.herokuapp.com", "localhost")
+            .exposedHeaders(
+                "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"
+            )
+            .allowCredentials(true)
     }
 }
