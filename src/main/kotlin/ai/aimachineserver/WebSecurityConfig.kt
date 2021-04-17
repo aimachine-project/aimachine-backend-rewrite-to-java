fd494a2ed2da6ee4f821dc9f8237bffff873da54
Sebastian Syska <syska.seb@gmail.com>
2021-04-17 23:54:30 +0200

    working ldap authentication

--- src/main/kotlin/ai/aimachineserver/WebSecurityConfig.kt
+++ src/main/kotlin/ai/aimachineserver/WebSecurityConfig.kt
@@ -3,11 +3,13 @@ package ai.aimachineserver
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 
 @Configuration
+@EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
