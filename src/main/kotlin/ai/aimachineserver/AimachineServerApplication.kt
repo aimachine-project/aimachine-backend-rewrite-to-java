e24760c73683a158929ccc491e478da4ba3310ea
Sebastian Syska <syska.seb@gmail.com>
2021-05-23 13:58:18 +0200

    add userservice and basic auth

--- src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
+++ src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
@@ -2,9 +2,14 @@ package ai.aimachineserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
-class AimachineServerApplication
+class AimachineServerApplication{
+    @Bean
+    fun encoder() = BCryptPasswordEncoder()
+}
 
 fun main(args: Array<String>) {
     runApplication<AimachineServerApplication>(*args)
--- src/main/kotlin/ai/aimachineserver/WebSecurityConfig.kt
+++ src/main/kotlin/ai/aimachineserver/WebSecurityConfig.kt
@@ -1,49 +1,42 @@
 package ai.aimachineserver
 
+import ai.aimachineserver.userlogin.UserDetailsServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
-import javax.servlet.Filter
-
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig(
+    private val passwordEncoder: PasswordEncoder
+) : WebSecurityConfigurerAdapter() {
 
-    @Bean
-    fun encoder() = BCryptPasswordEncoder()
-
-    @Autowired
-    private lateinit var userDetailsService: UserDetailsService
 
     @Autowired
-    private lateinit var authenticationEntryPoint: AuthEntryPointImpl
+    private lateinit var userDetailsService: UserDetailsServiceImpl
+
+    @Autowired
+    private lateinit var authProvider: AuthProvider
 
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth
+            .authenticationProvider(authProvider)
             .userDetailsService(userDetailsService)
-            .passwordEncoder(encoder())
+            .passwordEncoder(passwordEncoder)
     }
 
     override fun configure(http: HttpSecurity) {
         http
             .authorizeRequests()
-            .antMatchers("/", "/**").permitAll()
-            .antMatchers("/api/users/").authenticated()
-            .and()
-            .httpBasic().realmName("aimachine-backend")
-            .authenticationEntryPoint(authenticationEntryPoint)
+            .antMatchers("/api/users/**").access("hasAnyRole('USER')")
+            .and().httpBasic()
+//            .antMatchers("/", "/**").permitAll()
             .and().cors()
             .and().csrf().disable()
-//            .formLogin().disable()
+////            .formLogin().disable()
     }
 }
