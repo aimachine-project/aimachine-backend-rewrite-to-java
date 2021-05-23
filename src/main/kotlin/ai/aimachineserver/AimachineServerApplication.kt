9fa5159884a31fb856eff5ab184441a10d49a3f0
Sebastian Syska <syska.seb@gmail.com>
2021-05-23 18:34:45 +0200

    add userservice and basic auth

--- src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
+++ src/main/kotlin/ai/aimachineserver/AimachineServerApplication.kt
@@ -2,14 +2,9 @@ package ai.aimachineserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
-class AimachineServerApplication{
-    @Bean
-    fun encoder() = BCryptPasswordEncoder()
-}
+class AimachineServerApplication
 
 fun main(args: Array<String>) {
     runApplication<AimachineServerApplication>(*args)
