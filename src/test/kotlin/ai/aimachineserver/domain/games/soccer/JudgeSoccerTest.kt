74f75e7ec39dc4795fa623d99c3e0cd5899997c3
Sebastian Syska <s.syska@samsung.com>
2021-09-15 02:12:28 +0200

    fix formatting

--- src/test/kotlin/ai/aimachineserver/domain/games/soccer/JudgeSoccerTest.kt
+++ src/test/kotlin/ai/aimachineserver/domain/games/soccer/JudgeSoccerTest.kt
@@ -3,8 +3,6 @@ package ai.aimachineserver.domain.games.soccer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class JudgeSoccerTest {
 
     @Test
