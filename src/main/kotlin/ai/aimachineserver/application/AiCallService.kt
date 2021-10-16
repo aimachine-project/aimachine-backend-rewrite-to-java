7c1207f195bbae420a652ee4e58936b819f6f98a
Sebastian Syska <syska.seb@gmail.com>
2021-10-16 21:35:58 +0200

    ease dev with env variables

--- src/main/kotlin/ai/aimachineserver/application/AiCallService.kt
+++ src/main/kotlin/ai/aimachineserver/application/AiCallService.kt
@@ -1,19 +1,21 @@
 package ai.aimachineserver.application
 
+import ai.aimachineserver.AppConfig
 import ai.aimachineserver.application.commands.CallAiCommand
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
 @Service
-class AiCallService(private val restTemplate: RestTemplate) {
-    private companion object {
-        const val AI_URL = "http://ai:8081"
-    }
-
+@Configuration
+class AiCallService(
+    private val restTemplate: RestTemplate,
+    private val appConfig: AppConfig
+) {
     fun callAi(command: CallAiCommand): ResponseEntity<String> {
         val requestUrl =
-            "$AI_URL/${command.gameName}?requestedGameType=${command.requestedGameType}&gameId=${command.gameId}"
+            "${appConfig.aiServiceUrl}/${command.gameName}?requestedGameType=${command.requestedGameType}&gameId=${command.gameId}"
         return restTemplate.getForEntity(requestUrl, String::class.java)
     }
 }
