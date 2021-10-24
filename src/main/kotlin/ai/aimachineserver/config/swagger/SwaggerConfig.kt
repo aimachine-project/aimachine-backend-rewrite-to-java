cf0912afd7181cce0d4d6c2c3fda429ac081e33a
Sebastian Syska <syska.seb@gmail.com>
2021-10-24 04:18:48 +0200

    add basic auth to SwaggerConfig.kt

--- src/main/kotlin/ai/aimachineserver/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/ai/aimachineserver/config/swagger/SwaggerConfig.kt
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.BasicAuth
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
+
 
 @Configuration
 class SwaggerConfig {
@@ -16,5 +18,6 @@ class SwaggerConfig {
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build()
+            .securitySchemes(listOf(BasicAuth("basicAuth")))
     }
 }
